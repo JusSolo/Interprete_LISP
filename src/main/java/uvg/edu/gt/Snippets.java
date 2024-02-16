b68b0c210120fc1e816a89cf48b4d77bf60e05cc
JusSolo <Sol201598@uvg.edu.gt>
2024-02-15 19:14:02 -0600

    Se creo la Snippets para crear una lista de listas en la cual representa una "matriz" con un comando por linea y un caracter por "columna"

---
+++ src/main/java/uvg/edu/gt/Snippets.java
@@ -0,0 +1,56 @@
+package uvg.edu.gt;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ *
+ */
+public class Snippets {
+    private static List<String> fullLPL( List<String> programa){
+        Stack<String> parentesis = new Stack<>();
+        List<String> prog2 = new ArrayList<String>();
+        String l = "";
+        for (int i = 0; i < programa.size(); i++){
+            String linea = programa.get(i);
+            for (int j=0; j < linea.length(); j++ ){
+                char car = linea.charAt(j);
+                switch (car){
+                    case '(':
+                        parentesis.add("-");
+                        l = l + "( ";
+                        break;
+                    case ')':
+                        parentesis.pop();
+                        l = l + " )";
+                        break;
+                    default:
+                        l = l + car;
+                }
+
+
+                }
+            if (parentesis.isEmpty()){
+                prog2.add(l);
+                l = "";
+            }
+        }
+        return prog2;
+
+    }
+
+    public static List<List<String>> romperCodigo(List<String> programa){
+        programa = fullLPL(programa);
+        List<List<String>> pedasos = new ArrayList<>();
+        for (int i = 0; i < programa.size(); i++){
+            String[] linea = programa.get(i).split(" ");
+            pedasos.add(Arrays.asList(linea));
+            }
+        return pedasos;
+        }
+}
+
---
+++ src/test/java/uvg/edu/gt/SnippetsTest.java
@@ -0,0 +1,26 @@
+package uvg.edu.gt;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SnippetsTest {
+
+    @org.junit.jupiter.api.Test
+    void romperCodigo() {
+        List<String> codigo =  Arrays.asList("(","+ ","1 2",")","(* 1 23 4 5)","(+ (* ( + 1 2) 5", ")6)");
+        List<String> s1 = Arrays.asList("(","+","1" , "2",")");
+        List<String> s2 = Arrays.asList("(","*,", "1", "23", "45",")");
+        List<String> s3 = Arrays.asList("(","+", "(","*", "(" , "+", "1","2",")" ,"5", ")","6",")");
+        List<List<String>> esperado = new ArrayList<>();
+        esperado.add(s1);
+        esperado.add(s2);
+        esperado.add(s3);
+        List<List<String>> actual = Snippets.romperCodigo(codigo);
+        String x1 = actual.toString();
+        String x2 = esperado.toString();
+        assertEquals(x2,x1);
+    }
+}
