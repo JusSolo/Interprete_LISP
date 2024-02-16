2462da2cb5eed93209605207989a11e895372ef4
JusSolo <Sol201598@uvg.edu.gt>
2024-02-15 19:14:02 -0600

    Se creo la Snippets para crear una lista de listas en la cual representa una "matriz" con un comando por linea y un caracter por "columna"

---
+++ src/test/java/uvg/edu/gt/SnippetsTest.java
@@ -0,0 +1,29 @@
+package uvg.edu.gt;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SnippetsTest {
+
+    @org.junit.jupiter.api.Test
+    void romperCodigo() {
+        List<String> codigo =  Arrays.asList("(","+ ","1 2",")","(* 1 23 45)","(+ (* ( + 1 2) 5", ")6) (SETQ mal \"toto\" )");
+        List<String> s1 = Arrays.asList("(","+","1" , "2",")");
+        List<String> s2 = Arrays.asList("(","*", "1", "23", "45",")");
+        List<String> s3 = Arrays.asList("(","+", "(","*", "(" ,"+", "1","2",")" ,"5", ")","6",")");
+        List<String> s4 = Arrays.asList("(","setq","mal", "\"toto\"",")");
+        List<List<String>> esperado = new ArrayList<>();
+        esperado.add(s1);
+        esperado.add(s2);
+        esperado.add(s3);
+        esperado.add(s4);
+
+        List<List<String>>  actual = Snippets.romperCodigo(codigo);
+        String x1 = actual.toString();
+        String x2 = esperado.toString();
+        assertEquals(x2,x1);
+    }
+}
